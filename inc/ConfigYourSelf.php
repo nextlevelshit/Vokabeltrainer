dffa60ad5ece119de7170fbd1e4a07cd7e39f8ee
Next Level Shit <michaelwerner@mailoo.org>
2015-01-28 18:37:55 +0100

    Test Commit

--- inc/ConfigYourSelf.php
+++ inc/ConfigYourSelf.php
@@ -2,6 +2,24 @@
 /**
  * Created by PhpStorm.
  * User: nextlevelshit
- * Date: 28.01.15
- * Time: 18:32
- */ 
+ * Date: 29.10.14
+ * Time: 01:33
+ */
+
+define("DEBUG", (boolean) $_REQUEST['debug']);
+
+define("_PAGE",             (string) $_REQUEST['page']);
+define("_LANGUAGE",         (int) $_REQUEST['lang']);
+define("_LANGUAGE_PAIR",    (int) $_REQUEST['language_pair']);
+define("_LEVEL",            (int) $_REQUEST['level']);
+define("_FUNC",             (string) $_REQUEST['func']);
+define("_TABLE",            (string) $_REQUEST['table']);
+define("_BOX",              (int) $_REQUEST['box']);
+
+define("CONF_LEVELS",       5); /* How many Learning Levels do you need? */
+
+/* SQL Stuff */
+define("SQL_USERNAME",      "root");
+define("SQL_PASSWORD",      "");
+define("SQL_HOSTNAME",      "localhost");
+define("SQL_TABLE",         "trainer");
