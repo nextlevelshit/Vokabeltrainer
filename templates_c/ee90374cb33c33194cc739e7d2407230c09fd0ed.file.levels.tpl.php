2efa32c7ddeb77ba77494295a0234e77a5fcd6aa
Next Level Shit <michaelwerner@mailoo.org>
2015-01-29 16:56:54 +0100

    Exclude .idea

--- templates_c/ee90374cb33c33194cc739e7d2407230c09fd0ed.file.levels.tpl.php
+++ templates_c/ee90374cb33c33194cc739e7d2407230c09fd0ed.file.levels.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-01-22 20:31:38
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-01-28 19:44:45
          compiled from "templates/levels.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:114605665654529ebc0138e8-22222221%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,13 +7,13 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'ee90374cb33c33194cc739e7d2407230c09fd0ed' => 
     array (
       0 => 'templates/levels.tpl',
-      1 => 1414753090,
+      1 => 1422465898,
       2 => 'file',
     ),
     'ebfc53871d7e8f4ec0f2f455926384d80be09f3c' => 
     array (
       0 => './templates/_layout.tpl',
-      1 => 1421954687,
+      1 => 1422470425,
       2 => 'file',
     ),
   ),
@@ -34,10 +34,11 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <head lang="de">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSS -->
-    <link rel="stylesheet" href="assets/css/bootstrap.css"/>
-    <link rel="stylesheet" href="assets/css/GGS.css"/>
-    <!--<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">-->
+    
+    
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/styles.css"/>
     <link rel="stylesheet" href="assets/css/jquery.mmenu.all.css"/>
     <!-- JS -->
@@ -50,9 +51,9 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     <!--<?php echo '<script'; ?>
  src="assets/js/bootstrap.min.js"><?php echo '</script'; ?>
 >-->
-    <!--<?php echo '<script'; ?>
+    <?php echo '<script'; ?>
  src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"><?php echo '</script'; ?>
->-->
+>
     <?php echo '<script'; ?>
  src="assets/js/scripts.js"><?php echo '</script'; ?>
 >
@@ -65,8 +66,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 >
     <![endif]-->
     <title>Mein Vokabeltrainer</title>
-    <link href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700&subset=latin,latin-ext" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700|Pacifico&subset=latin,greek-ext,greek,latin-ext,cyrillic-ext,cyrillic' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
@@ -85,49 +85,62 @@ $_valid = $_smarty_tpl->decodeProperties(array (
                 </ul>
             </li>
             <li class="dropdown">
-                <a href="#">test</a>
+                <span><a href="#">Bearbeiten</a></span>
                 <ul>
-                    <li><a href="#">test</a></li>
-                    <li><a href="#">test</a></li>
-                    <li><a href="#">test</a></li>
+                    <li><a href="?page=edit&table=words&func=add">Neue Karteikarten</a></li>
+                    <li><a href="?page=edit&table=words&func=edit">Karteikarten bearbeiten</a></li>
+                    
                 </ul>
             </li>
         </ul>
     </div>
 </nav>
-
-
-<div class="header__band">
-    <header>
-    <div class="wrapper">
-        <h1><span class="glyphicon glyphicon-inbox"></span> Mein Vokabeltrainer</h1>
+<nav id="footer__menu">
+    <div clas="wrapper">
+        
     </div>
-    </header>
-</div>
-<div class="header__shadow">
-    <header>
-        <div class="wrapper">
-            <h3>
+</nav>
+
+<header>
+    <div class="header__band">
+        <div class="container">
+            <div class="row">
+                <h1>
+                    <a href="index.php">
+                        <span class="glyphicon glyphicon-inbox"></span> Mein Vokabeltrainer
+                    </a>
+                </h1>
+            </div>
+        </div>
+    </div>
+    <div class="header__shadow">
+        <div class="container">
+            <div class="row">
+                <h3>
     <a href="?page=languages">&laquo; Zurück zur Sprachwahl</a>
 </h3>
+            </div>
         </div>
-    </header>
-</div>
+    </div>
+</header>
 
-<article id="oneway">
-    <p>
+<article class="container">
+    <div class="row">
+            <div class="col-xs-12">
+            <p>
     Bitte wähle eine Stufe, die du gerne üben möchtest.
 </p>
 
-    <div class="alert alert-danger"><?php echo $_smarty_tpl->tpl_vars['alert']->value;?>
+            <div class="alert alert-danger"><?php echo $_smarty_tpl->tpl_vars['alert']->value;?>
 </div>
-    <div class="doublemargin"></div>
-</article>
+            <div class="doublemargin"></div>
+        </div>
+    </div>
 
-
-    <article id="twoway" class="levels">
-        <section class="wrapper">
-            <ul>
+    
+    <section class="row levels">
+        <div class="col-sm-6">
+            <ul class="levels__list">
             <?php  $_smarty_tpl->tpl_vars['box'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['box']->_loop = false;
  $_from = $_smarty_tpl->tpl_vars['boxes']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
 foreach ($_from as $_smarty_tpl->tpl_vars['box']->key => $_smarty_tpl->tpl_vars['box']->value) {
@@ -144,21 +157,32 @@ $_smarty_tpl->tpl_vars['box']->_loop = true;
                 </li>
             <?php } ?>
             </ul>
-        </section>
-        <section class="wrapper">
+        </div>
+        <div class="col-sm-6">
             <button class="btn btn-transparent">Neue Karteikarten hinzufügen</button>
             <div class="margin"></div>
             <button class="btn btn-transparent">Karteikarten bearbeiten</button>
-        </section>
-    </article>
+        </div>
+    </section>
 
-    <footer>
-        <article id="oneway">
-            <section class="wrapper">
+</article>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 hidden-xs">
                 
-            </section>
-        </article>
-    </footer>
+            </div>
+            
+                
+                    
+                
+                
+                    
+                
+            
+        </div>
+    </div>
+</footer>
 
 <?php echo $_smarty_tpl->getSubTemplate ("includes/footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
 
